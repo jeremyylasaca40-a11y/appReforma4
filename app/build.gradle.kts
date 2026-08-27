6bcd41efc8dda7b4d8fdf13b268a17ceba1bdc0a
Jeremy Ylasaca <jeremyylasaca26@gmail.com>
2026-08-26 21:10:14 -0500

    agregando el diseño de la aplicacion y los colores

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,10 @@ android {
 }
 
 dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
